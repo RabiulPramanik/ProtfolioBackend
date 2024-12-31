73a505c391b21a22f1644771bce163553e43762e
rabiul_pramanik <robiul.drubok@gmail.com>
2024-12-31 14:42:08 +0600

    four commit

--- BackendProtfolo/settings.py
+++ BackendProtfolo/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'contact',
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +77,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'BackendProtfolo.wsgi.application'
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500', 
+    'http://localhost:5500',
+]
 
 
 # Database
